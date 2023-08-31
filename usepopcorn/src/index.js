e820073d33f60bf0faa387c19304ee27b1883341
Paweł <pavelturczynski@gmail.com>
2023-08-31 21:14:55 +0200

    add star comp

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(1);
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        defaultRating={1}
+        onSetRating={setMovieRating}
+      ></StarRating>
+      <p>Rating is {movieRating}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Test></Test>
   </React.StrictMode>
 );
 
